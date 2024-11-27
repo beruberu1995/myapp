53ef227b36686dc95c971ac1f771691300ad2547
buakong05171329 <buakong05171329@gmail.com>
2024-11-27 08:39:04 +0000

    App Bar of the Home Page

---
+++ lib/pages/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Breakfast",
+          style: TextStyle(
+              color: Colors.green, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
+
+//Stateless and Stateful in flutter
